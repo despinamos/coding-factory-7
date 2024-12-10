450656130a455c8fbe064c84d0c0babc60b70709
D. Moschokarfi <despinamx16@gmail.com>
2024-12-10 20:30:04 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * this is a documentation comment (java-doc)
+ * Prints Hello Coding Factory in
+ * standard output (console)
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // this is an implementation comment
+        // Prints Hello Coding Factory
+        System.out.println("Hello Coding Factory!");
+    }
+}
