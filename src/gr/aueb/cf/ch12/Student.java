8a8809bf2bc1bfe57c3f8c94372acc8491dc4fc8
D. Moschokarfi <despinamx16@gmail.com>
2024-12-23 19:27:14 +0200

    object oriented

--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -1,9 +1,51 @@
 package gr.aueb.cf.ch12;
 
+/**
+ * Data class.
+ * POJO (Plain Old Java Object)
+ * Java Bean -> private fields, default constructor,
+ * setters and getters with convention
+ */
+
 public class Student {
     int id;
     String firstname;
     String lastname;
 
+    // Default Constructor
+    public Student() {
+
+    }
+
+    public Student(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
 }
--- src/gr/aueb/cf/ch12/StudentDemo.java
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -1,7 +1,22 @@
 package gr.aueb.cf.ch12;
 
+import java.sql.SQLOutput;
+
 public class StudentDemo {
     public static void main(String[] args) {
-        Student alice = new Student();
+        Student student = new Student();
+        Student bob = new Student(1, "Bob", "D.");
+
+        student.id = 1;
+        student.firstname = "Alice";
+        student.lastname = "W.";
+
+//        System.out.println("Id:" + student.id);
+//        System.out.println("Firstname: " + student.firstname);
+//        System.out.println("Lastname:" +student.lastname);
+
+        System.out.println("Id: " + student.getId());
+        System.out.println("Firstname: " + student.getFirstname());
+        System.out.println("Lastname: " + student.getLastname());
     }
 }
