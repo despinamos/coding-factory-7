b05ede56b6276f312a3d209cc318c9148a4ebf9a
D. Moschokarfi <despinamx16@gmail.com>
2025-01-03 18:43:48 +0200

    new stuff

--- src/gr/aueb/cf/ch12/model/Teacher.java
+++ src/gr/aueb/cf/ch12/model/Teacher.java
@@ -1,4 +1,46 @@
 package gr.aueb.cf.ch12.model;
 
+/**
+ * Java Bean.
+ */
+
 public class Teacher {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Teacher() {
+
+    }
+
+    public Teacher(int id, String lastname, String firstname) {
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
